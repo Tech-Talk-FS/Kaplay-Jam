fc352ed5f073f3dd24154797390cc891e3e67244
Carlos Mendez <freakinboyo@student.fullsail.edu>
2024-11-09 02:17:47 -0400

    Update player.js
    Added attack hitbox that only displays during the sword swipe part of the "attack" animation. Temporarily added debug info.

--- mygame/src/player.js
+++ mygame/src/player.js
@@ -32,12 +32,25 @@ export function createPlayer() {
         },
     });
 
+    // Creates the player sprite
     const player = add([
         sprite("player"),
         pos(center()),
         anchor("center"),
-        area(),
+        area({
+            shape: new Rect(vec2(0, -2), 13, 19)
+        }),
         body(),
+    ]);
+
+    // Attaches attack hurtbox onto player
+    const attack = player.add([
+        named("player_attack"),
+        anchor("left"),
+        rotate(0),
+        area({
+            shape: new Rect(vec2(0, 0), 22, 23)
+        }),
     ]);
 
     player.onButtonDown(["left", "right", "up", "down"], (button) => {
@@ -50,11 +63,13 @@ export function createPlayer() {
         if (button === "left") {
             xMod = -1;
             player.flipX = true;
+            attack.rotateTo(180);
         }
         // Move and face character right if the "right" button is pressed
         else if (button === "right") {
             xMod = 1;
             player.flipX = false;
+            attack.rotateTo(0);
         }
 
         // Move character up if the "up" button is pressed
@@ -72,28 +87,43 @@ export function createPlayer() {
         );
 
         // If the animation isn't already playing, play the "run" animation
-        if (player.getCurAnim().name !== "run" && player.getCurAnim().name !== "attack") {
+        if (player.getCurAnim()?.name !== "run" && player.getCurAnim()?.name !== "attack") {
             player.play("run");
         }
     });
 
+    // Plays the attack animation when the "attack" button is held down.
     player.onButtonDown("attack", (button) => {
-        if (player.getCurAnim().name !== button) {
+        if (player.getCurAnim()?.name !== button) {
             player.play(button);
         }
     })
 
     player.onUpdate(() => {
-        // If no movement button is pressed, play the "idle" animation
-        if (!player.getCurAnim() || (
-        player.getCurAnim().name !== "idle" &&
-        player.getCurAnim().name !== "attack" &&
+        attack.hidden = true;
+
+        // If no movement button is pressed and no "attack" animation is playing,
+        // play the "idle" animation
+        if (player.getCurAnim()?.name !== "idle" &&
+        player.getCurAnim()?.name !== "attack" &&
         !isButtonDown(["left", "right", "up", "down"])
-        )) {
+        ) {
             player.play("idle");
         }
+        
+        // If the "attack" animation reaches the sword swipe section,
+        // activate the attack hitbox.
+        if (player.getCurAnim()?.name === "attack" &&
+        (player.animFrame === 3 ||
+        player.animFrame === 4)) {
+            attack.hidden = false;
+        }
+
+        // Set camera to the player's position
         camPos(player.pos);
     });
+
+    debug.inspect = true;
 
     return player;
 }
