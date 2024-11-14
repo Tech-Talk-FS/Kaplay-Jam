5ac8928cf586a638c753f717302fc26a15a1865d
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-11-13 22:34:29 -0500

    Added sword assets to the hud ui. Also added health to hud.

--- mygame/src/hud/hud.js
+++ mygame/src/hud/hud.js
@@ -1,6 +1,13 @@
 
 
 export const loadHUD = () => loadSpriteAtlas("assests/hud-ui.png", {
+	heart: {
+		x: 0,
+		y: 0,
+		width: 64,
+		height: 16,
+		sliceX: 4
+	},
 	panel: {
 		x: 0,
 		y: 32,
@@ -12,6 +19,14 @@ export const loadHUD = () => loadSpriteAtlas("assests/hud-ui.png", {
 			right: 4,
 			bottom: 4
 		}
+	},
+	sword: {
+		x: 0,
+		y: 64,
+		width: 192,
+		height: 224,
+		sliceX: 6,
+		sliceY: 5
 	}
 })
 
@@ -20,26 +35,38 @@ class HUD {
 	lineLimit = 3
 	//messages will be added to a queue this will allow for key based scrolling
 	messageQueue = [];
-	constructor(){
-		this.$msg = add([
-			sprite('panel', {width: 128, height: 32}),
-			pos(width()/2, height()-16),
+	
+	constructor(player){
+		this.hp = 0
+		//this will be sort of a controlled component so static placement info only needs to be handled once 
+		this.$hud = add([
+			rect(width(), height(), {fill: false}),
+			outline(1, Color.fromHex(0xffff00)),
+			pos(center()),
 			anchor('center'),
-			fixed(),
+			fixed()
+		])
+		this.player = player;
+		this.$msg = this.$hud.add([
+			sprite('panel', {width: 128, height: 32}),
+			pos(0, height()/2),
+			anchor('bot'),
 		]);
 		this.$msg.hidden = true;
 		this.$text = this.$msg.add([
 			text("", {color:{r:255,g:255,b:255}, size: 4, align:'center'}),
-			anchor('center'),
+			pos(0, -12),
+			anchor('bot'),
 		]);
 		this.$msg.add([
 			//apparently cant set color in the initializer
 			text('ENTER to continue', {size: 3}),
-			anchor('center'),
-			pos(28, 9)
+			anchor('bot'),
+			pos(28, -5)
 		]).color = Color.fromHex(0x888888)
 
 		this.$msg.onKeyPress("enter", ()=>this.nextMessage());
+		player.onUpdate(this.updateHealth.bind(this));
 	}
 
 	immediateMsg(...values){
@@ -67,12 +94,45 @@ class HUD {
 	}
 
 	nextMessage = (isInitial=false)=>{
-		
 		if(!isInitial) this.messageQueue.shift();
-		console.log(this.messageQueue);
 		this.$text.text = this.messageQueue[0] ?? "";
 		this.$msg.hidden = !this.messageQueue.length;
 	}
+
+	updateHealth(){
+		const hp = this.player.hp();
+		if(this.hp === hp) return; //no need to update
+		this.hp = hp; 
+		if(this.$health){
+			this.$health.destroy();
+			delete this.$health;
+		}
+		
+		this.$health = this.$hud.add([
+			pos(-width()/2, height()/2),
+			anchor('bot')
+		]);
+		const fullHearts = Math.floor(hp/4);
+		const remainder = hp%4;
+		if(fullHearts){
+			const w = fullHearts*16;
+			this.$health.add([
+				sprite('heart', {frame: 3, tiled: true, width: w, height: 16}),
+				pos(0,0),
+				anchor('botleft')
+			])
+		}
+		if(remainder>0) this.$health.add([
+			sprite('heart', {frame: remainder-1, width: 16, height: 16}),
+			pos(fullHearts*16, 0),
+			anchor('botleft')
+		])
+	}
 }
 
-export const createHud = () => new HUD();
+/**
+ * Create hud object for the level
+ * @param {import("kaplay").GameObj} player 
+ * @returns 
+ */
+export const createHud = (player) => new HUD(player);
