ed8edfec2225726f64c82b55d0775ff8b948cba5
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-11-16 13:26:43 -0500

    fixed knockback vectoring on the hazard

--- mygame/src/objects/hazard.js
+++ mygame/src/objects/hazard.js
@@ -24,7 +24,7 @@ export const hazard = (damageAmount, knockback=0, attackDelay=1) => ({
 
 		this.onStateEnter('attack', async ()=>{
 			for(const c of this.getCollisions()){
-				if(c.target !== this && 'takeDamage' in c.target && typeof c.target.takeDamage === 'function') c.target.takeDamage(this.damageAmount, this.knockback, this.vec);
+				if(c.target !== this && 'takeDamage' in c.target && typeof c.target.takeDamage === 'function') c.target.takeDamage(this.damageAmount, this.knockback, this.pos.sub(c.target.pos).unit());
 			}
 			await wait(attackDelay);
 			this.enterState('idle');
