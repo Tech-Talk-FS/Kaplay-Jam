baccb37f275e56d873eee50589373e9a5522d68c
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-11-16 13:39:26 -0500

    centered the hitbox in hazardous

--- mygame/src/objects/hazardous.js
+++ mygame/src/objects/hazardous.js
@@ -14,6 +14,7 @@ export const hazardous = (damageAmount, knockback, attackDelay, hitbox) => ({
 		this.add([
 			area({shape: hitbox ?? new Rect(vec2(0,0), 16, 16)}),
 			state('idle', ['idle', 'attack']),
+			anchor('center'),
 			hazard(damageAmount, knockback, attackDelay)
 		])
 	}
