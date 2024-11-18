0c524110c33ead41957b8c200d4d8d52e585613a
Brad Beltowski <bradbeltowskigmail.com>
2024-11-18 01:00:48 -0500

    Switch direction

--- mygame/src/levels/b-level4.js
+++ mygame/src/levels/b-level4.js
@@ -13,26 +13,26 @@ const level19 = {
 	],
 	dungeon: [
 		"[=========]",
-		"[        E]",
-		"[ H       ]",
-		"[        S]",
+		"[E   S   E]",
 		"[         ]",
-		"[        E]",
-		",_________.",
+		"[         ]",
+		"[         ]",
+		"[         ]",
+		",___)_____.",
 	],
 	ornaments: [
 		"  !     !  ",
 		"     $     ",
-		"   @       ",
+		"          ",
 		"",
 		"",
-		"",
+		"    @",
 		"",
 	],
 	async setup() {
-		const [ladder] = DM.dungeon.get('door');
+		const [door] = DM.dungeon.get('door');
 		
-		ladder.interact = (player) => {DM.go(3,3)}
+		door.interact = (player) => {DM.go(15)}
 
 		const [skeleton] = DM.ornaments.get('skeleton');
 
@@ -58,34 +58,6 @@ const level19 = {
 			DM.player?.dialog(`With this torch, the web should pose no trouble.`);
 			DM.locals.torchUnlocked = true;
 			torch.destroy();
-		}
-
-		const [web] = DM.ornaments.get("web");
-
-		const [silverKey] = DM.dungeon.get('iron-key')
-
-		if (DM.locals.silverKey) {
-			silverKey.destroy();
-			web.destroy();
-		} else {
-			silverKey.interact = (player) => {
-				if (!DM.locals.torchUnlocked) {
-					DM.player?.dialog(`There's a key here, but the web's hold is too strong.`);
-				} else {
-					DM.player?.dialog(`A silver key...
-Within my grasp, I can't help but
-feel something stir.
-Could I have used it before?
-Was it to lock something in
-or keep something out?`);
-					DM.locals.silverKey = true;
-					silverKey.destroy();
-					web.destroy();
-	
-					skeleton.hidden = false;
-					skeleton.paused = false;
-				}
-			}
 		}
 	},
 	tiles: {
