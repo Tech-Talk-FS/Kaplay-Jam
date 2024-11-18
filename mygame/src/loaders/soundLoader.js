0f591005130ebab404bae311097af62c54f012c1
levelhlawrence <levelhlawrence@gmail.com>
2024-11-17 22:20:33 -0500

    updated sound files

--- mygame/src/loaders/soundLoader.js
+++ mygame/src/loaders/soundLoader.js
@@ -1,15 +1,16 @@
+export function soundLoader() {
+  // MUSIC BELOW
+  loadMusic("dungeon1", "./sounds/dungeon1.mp3"); // dungeon 1 music
 
-export function soundLoader () {
-
-   // MUSIC BELOW
-   loadMusic("dungeon1", "./sounds/dungeon1.mp3") // dungeon 1 music
-
-   // SOUND EFFECTS BELOW
-   loadSound("playerWalking", "./sounds/footstepcarpet001.ogg") // walking sound
-   loadSound("swordHit", "./sounds/wooshSoundEffect.mp3") // sword slashing sound
-   loadSound("interact", "./sounds/impactBellheavy002.ogg") // interacting with object sound
-   //loadSound("skeletonDeath", "enter route here") // no sound chosen
-   //loadSound("playerDeath", "enter route here") // no sound chosen
-   //loadSound("healthIncrease", "enter route here") // no sound chosen
+  // SOUND EFFECTS BELOW
+  loadSound("playerWalking", "./sounds/footstepcarpet001.ogg"); // walking sound
+  loadSound("swordHit", "./sounds/wooshSoundEffect.mp3"); // sword slashing sound
+  loadSound("interact", "./sounds/impactBellheavy002.ogg"); // interacting with object sound
+  loadSound("skeletonDeath", "./sounds/creatureDeath.mp3"); // skelton death
+  loadSound("playerDeath", "./sounds/playerDeath.mp3"); // playerDeath sounds
+  loadSound("upgrade", "./sounds/swordUpgrade.mp3"); // item upgrade sounds
+  loadSound("onHitOnEnemy", "./sounds/onHit.mp3"); // on hit
+  //loadSound("enemyAttack", "enter route here") // no sound chosen
+  //loadSound("slimeAttack", "enter route here") // no sound chosen
+  //loadSound("healthIncrease", "enter route here") // no sound chosen
 }
-
