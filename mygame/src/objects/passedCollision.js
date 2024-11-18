af997573f73e6f3b3e9d131a4d558d3ed54df457
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-11-17 23:28:47 -0500

    oops forgot to commit.

--- mygame/src/objects/passedCollision.js
+++ mygame/src/objects/passedCollision.js
@@ -8,6 +8,7 @@ export const passedCollision = (...collisions) => ({
 		this.collisionBox = this.add([
 			area({shape: this.shape ?? new Rect(vec2(0,0), 16, 16)})
 		])
-		for(const [tag, action] of collisions) this.collisionBox.onCollide(tag, action);
+		for(const [tag, action] of collisions) this.collisionBox.onCollide(tag, (...args)=>action(...args, this));
+		this.unuse('area');
 	}
 })
